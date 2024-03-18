6994b91b122f36a304e160f50eb3bfae6184c104
gnaroshi <gnaroshi@gmail.com>
2024-03-18 18:20:38 +0900

    20240318 [UPDATE] solvedac practice - window

--- With_Levels/Bronze/Bronze_1/7132.cpp
+++ With_Levels/Bronze/Bronze_1/7132.cpp
@@ -8,8 +8,20 @@ int main(void)
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int m, n;
+    int m, n, g;
     cin >> m >> n;
+    set<pair<int, int>> s;
+    for (int i = m; i <= n; i++)
+    {
+        for (int j = m; j <= n; j++)
+        {
+            g = __gcd(i, j);
+            s.insert({i / g, j / g});
+            s.insert({j / g, i / g});
+        }
+    }
+
+    cout << s.size();
 
     return 0;
 }
