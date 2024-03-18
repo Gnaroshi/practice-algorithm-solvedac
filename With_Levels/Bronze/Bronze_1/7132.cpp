ad88294362aec56a128ffe3ee92839c2984d8c9e
gnaroshi <gnaroshi@gmail.com>
2024-03-18 16:18:01 +0900

    20240318 [UPDATE] solvedac practice - window

---
+++ With_Levels/Bronze/Bronze_1/7132.cpp
@@ -0,0 +1,15 @@
+// problem: Hammasrattad
+// id: 7132
+// time taken:
+#include <bits/stdc++.h>
+using namespace std;
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int m, n;
+    cin >> m >> n;
+
+    return 0;
+}
