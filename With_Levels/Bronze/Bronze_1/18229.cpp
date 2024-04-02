a8253a08cf11deef61a46cb56cc413eda2d04106
Mingyu Jung <gnaroshi@Mingyus-MacBook-Pro.local>
2024-04-02 16:30:31 +0900

    20240402 [UPDATE] solvedac practice

---
+++ With_Levels/Bronze/Bronze_1/18229.cpp
@@ -0,0 +1,40 @@
+// problem: 내가 살게, 아냐 내가 살게
+// id: 18229
+// tag:
+// time taken:
+#include <bits/stdc++.h>
+using namespace std;
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m, k, t;
+    cin >> n >> m >> k;
+    vector<vector<int>> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> t;
+            v[i].push_back(t);
+        }
+    }
+
+    vector<int> cnt(n);
+    fill(cnt.begin(), cnt.end(), 0);
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (cnt[j] + v[j][i] >= k)
+            {
+                cout << j + 1 << ' ' << i + 1 << '\n';
+                return 0;
+            }
+            cnt[j] += v[j][i];
+        }
+    }
+
+    return 0;
+}
