3ef0c285c4e6388c31fbe69c5b461472513c3d6e
Mingyu Jung <gnaroshi@Mingyus-MacBook-Pro.local>
2024-04-14 22:37:07 +0900

    [UPDATE] 20240414 solvedac practice

---
+++ With_Levels/Bronze/Bronze_1/27865.cpp
@@ -0,0 +1,25 @@
+// problem: 랜덤 게임?
+// id: 27865
+// tag:
+// time taken:
+#include <bits/stdc++.h>
+using namespace std;
+int main(void)
+{
+    int n, ans;
+    char c;
+    cin >> n;
+    while (true)
+    {
+        cout << "? 1\n";
+        cin >> c;
+        if (c == 'Y')
+        {
+            cout << "! 1\n";
+            cout << flush;
+            return 0;
+        }
+    }
+
+    return 0;
+}
