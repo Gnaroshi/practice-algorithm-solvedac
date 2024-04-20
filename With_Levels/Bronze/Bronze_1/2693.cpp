fb47a596d8d79caa43b18eeb2c261b60f002dc6d
Mingyu Jung <gnaroshi@Mingyus-MacBook-Pro.local>
2024-04-20 21:28:09 +0900

    [UPDATE] 20240419 solvedac practice

---
+++ With_Levels/Bronze/Bronze_1/2693.cpp
@@ -0,0 +1,21 @@
+// problem: N번쨰 큰 수
+// id: 2693
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  return 0;
+}
