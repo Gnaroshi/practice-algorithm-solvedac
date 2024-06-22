fc16d6104647e4cb6ddda9410e5daa8a0f6b6e53
gnaroshi <gnaroshi@gmail.com>
2024-06-22 15:29:54 +0900

    20240622 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Bronze/Bronze_5/28938.cpp
@@ -0,0 +1,53 @@
+// problem: Конвейер
+// id: 28938
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, t, s = 0;
+  cin >> n;
+  while (n--) {
+    cin >> t;
+    s += t;
+  }
+  if (s > 0)
+    cout << "Right\n";
+  else if (s < 0)
+    cout << "Left\n";
+  else
+    cout << "Stay\n";
+
+  return 0;
+}
