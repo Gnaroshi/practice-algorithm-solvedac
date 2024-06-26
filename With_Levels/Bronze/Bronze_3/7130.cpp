77f4cc4a7e61584b4e580a53abcec107d0f8b1e8
gnaroshi <gnaroshi@gmail.com>
2024-06-26 14:52:53 +0900

    20240626 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Bronze/Bronze_3/7130.cpp
@@ -0,0 +1,51 @@
+// problem: Milk and Honey
+// id: 7130
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int m, h, n;
+  cin >> m >> h >> n;
+
+  int c, b, ans = 0;
+  for (int i = 0; i < n; i++) {
+    cin >> c >> b;
+    ans += max(c * m, b * h);
+  }
+
+  cout << ans << "\n";
+
+  return 0;
+}
