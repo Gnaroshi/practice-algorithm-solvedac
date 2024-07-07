ac72a021b7070d6401cea6daded6ff375e220c44
gnaroshi <gnaroshi@gmail.com>
2024-07-07 09:42:14 +0900

    20240707 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Bronze/Bronze_3/30821.cpp
@@ -0,0 +1,52 @@
+// problem: 별자리가 될 수 있다면
+// id: 30821
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  ll ans = 1, t = 1;
+
+  // n!/(n-5)!5!
+  for (ll i = 0; i < 5; i++) {
+    ans *= (n - i);
+    t *= (i + 1);
+  }
+
+  cout << ans / t << '\n';
+
+  return 0;
+}
