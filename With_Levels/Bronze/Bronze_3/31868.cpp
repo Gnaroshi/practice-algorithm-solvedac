15eed77351fec8f23b932acd2df0ea4b537e6ea9
gnaroshi <gnaroshi@gmail.com>
2024-07-25 18:27:50 +0900

    20240725 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Bronze/Bronze_3/31868.cpp
@@ -0,0 +1,49 @@
+// problem: 수박 게임
+// id: 31868
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  cin >> n >> k;
+
+  for (int i = 1; i < n; i++) {
+    k /= 2;
+  }
+  cout << k << '\n';
+
+  return 0;
+}
