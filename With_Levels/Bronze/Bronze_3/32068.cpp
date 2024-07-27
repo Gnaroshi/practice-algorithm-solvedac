6ef1b6de50c46eefcda5ee809b20bcbc5c760398
gnaroshi <gnaroshi@gmail.com>
2024-07-27 23:10:35 +0900

    20240727 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Bronze/Bronze_3/32068.cpp
@@ -0,0 +1,53 @@
+// problem: 보물 찾기
+// id: 32068
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int l, r, s;
+    cin >> l >> r >> s;
+    if (r - s <= s - l)
+      cout << (r - s) * 2;
+    else
+      cout << (s - l) * 2 + 1;
+    cout << '\n';
+  }
+
+  return 0;
+}
