62f939451b39a348303120b86e9a76ca84c9aa44
gnaroshi <gnaroshi@gmail.com>
2024-08-02 17:05:08 +0900

    20240802 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Bronze/Bronze_3/30596.cpp
@@ -0,0 +1,45 @@
+// problem: Axis-Aligned Area
+// id: 30596
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  cout << min(a, b) * min(c, d) << '\n';
+
+  return 0;
+}
