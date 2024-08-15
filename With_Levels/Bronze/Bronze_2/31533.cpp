28d8c931852fa9abefda2735aae56558100e331e
gnaroshi <gnaroshi@gmail.com>
2024-08-15 10:55:46 +0900

    20240815 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Bronze/Bronze_2/31533.cpp
@@ -0,0 +1,47 @@
+// problem: Furiosa AI 영상 처리 가속
+// id: 31533
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  double a, m, n;
+  cin >> a >> m >> n;
+  cout << fixed;
+  cout.precision(7);
+  cout << min(min(m, n) / a * 2, max(max(m, n) / a, min(m, n))) << '\n';
+
+  return 0;
+}
