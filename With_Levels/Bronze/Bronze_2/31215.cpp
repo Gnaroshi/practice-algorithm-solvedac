f0005f24419c7041bebba3e9bee6c09f6e424ebb
gnaroshi <gnaroshi@gmail.com>
2024-08-16 23:02:58 +0900

    20240816 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Bronze/Bronze_2/31215.cpp
@@ -0,0 +1,53 @@
+// problem: 이상한 섞기 연산
+// id: 31215
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int n;
+    cin >> n;
+    if (n < 3)
+      cout << 1;
+    else
+      cout << 3;
+    cout << '\n';
+  }
+
+  return 0;
+}
