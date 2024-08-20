b8cc69474e475503b1ab6d49ee73ba4e476ab178
gnaroshi <gnaroshi@gmail.com>
2024-08-20 17:05:25 +0900

    20240820 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Bronze/Bronze_2/31995.cpp
@@ -0,0 +1,50 @@
+// problem: 게임말 올려놓기
+// id: 31995
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+
+  if (n < 2 || m < 2)
+    cout << 0 << '\n';
+  else {
+    cout << (n - 1) * (m - 1) * 2 << '\n';
+  }
+
+  return 0;
+}
