f53b99b351c3888f30c57b74f3873bc767884a48
gnaroshi <gnaroshi@gmail.com>
2024-09-28 20:40:05 +0900

    20240928 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Bronze/Bronze_1/19945.cpp
@@ -0,0 +1,56 @@
+// problem: 새로운 언어 CC
+// id: 19945
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  if (n < 0)
+    cout << 32 << '\n';
+  else if (n == 0)
+    cout << 1 << '\n';
+  else {
+    int cnt = 0;
+    while (n) {
+      n /= 2;
+      cnt++;
+    }
+    cout << cnt << '\n';
+  }
+
+  return 0;
+}
