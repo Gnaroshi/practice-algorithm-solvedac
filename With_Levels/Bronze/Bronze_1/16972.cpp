add2f39e1a5736107283ccc8d4e4857a8ec1704c
gnaroshi <gnaroshi@gmail.com>
2024-10-20 12:25:59 +0900

    20241020 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Bronze/Bronze_1/16972.cpp
@@ -0,0 +1,47 @@
+// problem: 814 - 1
+// id: 16972
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  for (int i = 0; i < 813; i++) {
+    cout << 10 * i << ' ' << 10 * i << '\n';
+  }
+
+  cout << -1 << ' ' << -1 << '\n';
+
+  return 0;
+}
