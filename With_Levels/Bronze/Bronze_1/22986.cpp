8781d507d467750b42a67baf5c7bab46bfd3c395
gnaroshi <gnaroshi@gmail.com>
2024-10-24 14:23:27 +0900

    20241024 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Bronze/Bronze_1/22986.cpp
@@ -0,0 +1,50 @@
+// problem: Flat Earth
+// id: 22986
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    ll n, k;
+    cin >> n >> k;
+    k = min(n, k);
+    cout << 2 * (k + 1) * (2 * n - k) << '\n';
+  }
+
+  return 0;
+}
