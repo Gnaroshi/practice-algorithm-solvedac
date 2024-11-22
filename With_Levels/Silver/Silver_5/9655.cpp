d1e1a954a8c6d4074ade1a7b33059b7561feaa0f
gnaroshi <gnaroshi@gmail.com>
2024-11-22 12:51:34 +0900

    20241123 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_5/9655.cpp
@@ -0,0 +1,47 @@
+// problem: 돌 게임
+// id: 9655
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  cout << (n % 2 == 1 ? "SK\n" : "CY\n");
+
+  return 0;
+}
