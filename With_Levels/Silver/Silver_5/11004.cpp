0be747b9676d77433694a77d5773c3e731092761
gnaroshi <gnaroshi@gmail.com>
2024-11-24 19:51:32 +0900

    20241125 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_5/11004.cpp
@@ -0,0 +1,51 @@
+// problem: K번째 수
+// id: 11004
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  cin >> n >> k;
+  vi v(n);
+  for (auto &i : v)
+    cin >> i;
+  sort(v.begin(), v.end());
+  cout << v[k - 1] << '\n';
+
+  return 0;
+}
