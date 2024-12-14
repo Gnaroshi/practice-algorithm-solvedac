0de8abb6c6b925af8696e81edae311767a8d29cd
gnaroshi <gnaroshi@gmail.com>
2024-12-14 18:05:27 +0900

    20241214 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_5/16435.cpp
@@ -0,0 +1,58 @@
+// problem: 스네이크버드
+// id: 16435
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, l;
+  cin >> n >> l;
+  vi v(n);
+  for (auto &i : v)
+    cin >> i;
+  sort(v.begin(), v.end());
+  for (auto i : v) {
+    if (l >= i) {
+      l++;
+    } else
+      break;
+  }
+
+  cout << l << '\n';
+
+  return 0;
+}
