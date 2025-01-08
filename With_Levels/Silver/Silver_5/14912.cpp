e31b352e9cf874db378aaafb8ec222915532d62c
gnaroshi <gnaroshi@gmail.com>
2025-01-08 19:23:16 +0900

    20250108 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_5/14912.cpp
@@ -0,0 +1,56 @@
+// problem: 숫자 빈도수
+// id: 14912
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, d, ans = 0, t;
+  cin >> n >> d;
+  for (int i = 1; i <= n; i++) {
+    t = i;
+    while (t) {
+      if (t % 10 == d)
+        ans++;
+      t /= 10;
+    }
+  }
+
+  cout << ans << "\n";
+
+  return 0;
+}
