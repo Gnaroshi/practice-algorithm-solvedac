b16c4ad59763ee3f299c4b65db0d0315db5a504f
gnaroshi <gnaroshi@gmail.com>
2025-01-09 17:21:08 +0900

    20250109 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_5/3135.cpp
@@ -0,0 +1,52 @@
+// problem: 라디오
+// id: 3135
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b, n;
+  cin >> a >> b >> n;
+  int mn = abs(b - a), t;
+  for (int i = 0; i < n; i++) {
+    cin >> t;
+    mn = min(mn, abs(b - t) + 1);
+  }
+  cout << mn << '\n';
+
+  return 0;
+}
