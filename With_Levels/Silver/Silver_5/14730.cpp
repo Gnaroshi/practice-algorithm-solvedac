d7389593f44c0b4877f31bb0b5e52e66df227b03
gnaroshi <gnaroshi@gmail.com>
2025-02-02 20:49:15 +0900

    20250202 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_5/14730.cpp
@@ -0,0 +1,53 @@
+// problem: 謎紛芥索紀 (Small)
+// id: 14730
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, ans = 0, a, b;
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    cin >> a >> b;
+    ans += a * b;
+  }
+
+  cout << ans << '\n';
+
+  return 0;
+}
