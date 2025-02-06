04d3b8311a836a5e2206c33f4ad9c428cb2f6346
gnaroshi <gnaroshi@gmail.com>
2025-02-06 14:38:39 +0900

    20250206 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_5/15719.cpp
@@ -0,0 +1,52 @@
+// problem: 중복된 숫자
+// id: 15719
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll n, s, t = 0;
+  cin >> n;
+  s = (n * (n + 1)) / 2;
+  for (int i = 0; i < n; i++) {
+    cin >> t;
+    s -= t;
+  }
+  cout << n - s << '\n';
+
+  return 0;
+}
