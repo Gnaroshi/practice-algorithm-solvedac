39106d5e8aaeed7494f61c9929de5bd83e56d147
gnaroshi <gnaroshi@gmail.com>
2025-02-14 17:43:13 +0900

    20250214 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_5/3060.cpp
@@ -0,0 +1,60 @@
+// problem: 욕심쟁이 돼지
+// id: 3060
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    ll n, s = 0, ans = 1, t;
+    cin >> n;
+    for (int i = 0; i < 6; i++) {
+      cin >> t;
+      s += t;
+    }
+    while (n >= s) {
+      ans++;
+      s *= 4;
+    }
+
+    cout << ans << '\n';
+  }
+
+  return 0;
+}
