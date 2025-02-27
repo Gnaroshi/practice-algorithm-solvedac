21ce0ac136e224df00855b4d35e8ec535d058abf
gnaroshi <gnaroshi@gmail.com>
2025-02-27 18:37:40 +0900

    20250227 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_5/14655.cpp
@@ -0,0 +1,57 @@
+// problem: 욱제는 도박쟁이야!!
+// id: 14655
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  int ans = 0, t;
+  for (int i = 0; i < n; i++) {
+    cin >> t;
+    ans += abs(t);
+  }
+  for (int i = 0; i < n; i++) {
+    cin >> t;
+    ans += abs(t);
+  }
+
+  cout << ans << '\n';
+
+  return 0;
+}
