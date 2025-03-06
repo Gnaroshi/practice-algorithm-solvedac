5c1af5104140f1ad8d34faf3ef758881e46a8961
gnaroshi <gnaroshi@gmail.com>
2025-03-07 00:55:50 +0900

    20250306 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_5/21760.cpp
@@ -0,0 +1,59 @@
+// problem: 야구 시즌
+// id: 21760
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    ll n, m, k, d;
+    cin >> n >> m >> k >> d;
+
+    ll t = n * m * (k * (m - 1) + (n * m - m)) / 2;
+    ll ans = d / t;
+    if (ans == 0) {
+      ans = -1;
+    } else {
+      ans *= t;
+    }
+    cout << ans << '\n';
+  }
+
+  return 0;
+}
