4e2969aa5456a4d9b35ad2bd658f098f92fc2541
gnaroshi <gnaroshi@gmail.com>
2025-04-21 13:03:25 +0900

    20250421 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_4/9372.cpp
@@ -0,0 +1,55 @@
+// problem: 상근이의 여행
+// id: 9372
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int n, m, a, b;
+    cin >> n >> m;
+    vb chk(n, false);
+    for (int i = 0; i < m; i++) {
+      cin >> a >> b;
+    }
+    cout << n - 1 << '\n';
+  }
+
+  return 0;
+}
