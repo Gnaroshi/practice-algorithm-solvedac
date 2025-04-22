7515db929d5d588057426d0498100fa7303aff4d
gnaroshi <gnaroshi@gmail.com>
2025-04-22 23:12:10 +0900

    20250422 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_4/3036.cpp
@@ -0,0 +1,51 @@
+// problem: 링
+// id: 3036
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, a, b;
+  cin >> n >> a;
+  for (int i = 1; i < n; i++) {
+    cin >> b;
+    int g = gcd(a, b);
+    cout << a / g << "/" << b / g << '\n';
+  }
+
+  return 0;
+}
