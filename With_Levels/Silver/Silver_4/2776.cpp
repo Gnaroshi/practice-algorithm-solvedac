d36274df8f09ddff9ee2b9359de55062256647f2
gnaroshi <gnaroshi@gmail.com>
2025-04-25 22:44:36 +0900

    20250425 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_4/2776.cpp
@@ -0,0 +1,65 @@
+// problem: 암기왕
+// id: 2776
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int n, m, t;
+    cin >> n;
+    map<int, bool> mp;
+    for (int i = 0; i < n; i++) {
+      cin >> t;
+      mp[t] = true;
+    }
+
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+      cin >> t;
+      if (mp.find(t) != mp.end()) {
+        cout << 1 << '\n';
+      } else {
+        cout << 0 << '\n';
+      }
+    }
+  }
+
+  return 0;
+}
