c61796f0bb3a1bf688c10af4ff3cd97582ab4c0e
gnaroshi <gnaroshi@gmail.com>
2025-05-05 15:56:19 +0900

    20250505 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_4/2670.cpp
@@ -0,0 +1,65 @@
+// problem: 연속부분최대곱
+// id: 2670
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vd v(n);
+  double mx = 0, t = 1;
+  for (auto &i : v) {
+    cin >> i;
+  }
+
+  mx = v[0];
+
+  for (int i = 0; i < n; i++) {
+    t = v[i];
+    for (int j = i + 1; j < n; j++) {
+      mx = max(mx, t);
+      t *= v[j];
+    }
+  }
+
+  cout << fixed;
+  cout.precision(3);
+  cout << mx << "\n";
+
+  return 0;
+}
