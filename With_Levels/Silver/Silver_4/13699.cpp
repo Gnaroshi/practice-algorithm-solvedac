96d82983eecaf6cb2efe3651ee9a4c2eba335be0
gnaroshi <gnaroshi@gmail.com>
2025-05-14 15:50:43 +0900

    20250514 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_4/13699.cpp
@@ -0,0 +1,58 @@
+// problem: 점화식
+// id: 13699
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  vll v(36, 0);
+  int n;
+  cin >> n;
+  v[0] = 1;
+  v[1] = 1;
+  for (int i = 2; i <= n; i++) {
+    ll t = 0;
+    for (int j = 0; j < i; j++) {
+      t += v[j] * v[i - j - 1];
+    }
+    v[i] = t;
+  }
+
+  cout << v[n] << "\n";
+
+  return 0;
+}
