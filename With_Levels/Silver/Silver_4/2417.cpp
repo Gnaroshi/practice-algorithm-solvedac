23c6ebe1d0d89ec9083c14b5ce3286720a002cb6
gnaroshi <gnaroshi@gmail.com>
2025-05-18 19:59:53 +0900

    20250518 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_4/2417.cpp
@@ -0,0 +1,53 @@
+// problem: 정수 제곱근
+// id: 2417
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll n, ans;
+  cin >> n;
+
+  ans = sqrt(n);
+  if (ans * ans < n) {
+    ans++;
+  }
+
+  cout << ans << '\n';
+
+  return 0;
+}
