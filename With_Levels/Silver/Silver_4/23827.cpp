87f90fe2866aa38279d31da46e138707a2a13947
gnaroshi <gnaroshi@gmail.com>
2025-07-27 21:44:57 +0900

    20250726 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_4/23827.cpp
@@ -0,0 +1,54 @@
+// problem: 수열 (Easy)
+// id: 23827
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll n, t, cur = 0, ans = 0, md = 1000000007;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> t;
+    ans += (cur * t);
+    cur += t;
+    ans %= md;
+  }
+
+  cout << ans << '\n';
+
+  return 0;
+}
