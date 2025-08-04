4240b1af31b3fa6b4a6646292efc32aa3241e010
gnaroshi <gnaroshi@gmail.com>
2025-08-04 11:56:46 +0900

    20250804 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_4/20363.cpp
@@ -0,0 +1,53 @@
+// problem: 당근 키우기
+// id: 20363
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll a, b, ca = 0, cb = 0, ans = 0;
+  cin >> a >> b;
+  if (a < b) {
+    swap(a, b);
+  }
+
+  ans += (a + b);
+  ans += (b / 10);
+  cout << ans << "\n";
+
+  return 0;
+}
