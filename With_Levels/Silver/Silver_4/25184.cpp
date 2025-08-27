3647eff03646e22caed966356a6e970f381dbedd
gnaroshi <gnaroshi@gmail.com>
2025-08-27 13:25:33 +0900

    20250827 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_4/25184.cpp
@@ -0,0 +1,55 @@
+// problem: 동가수열 구하기
+// id: 25184
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    if (i % 2 == 0) {
+      cout << i / 2 << ' ';
+    } else {
+      cout << n / 2 + i / 2 + 1 << ' ';
+    }
+  }
+
+  cout << '\n';
+
+  return 0;
+}
