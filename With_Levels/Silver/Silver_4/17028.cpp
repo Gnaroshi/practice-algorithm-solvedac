b6ef515c923dbe5a63e14d7ed7e5c85a57f0ec35
gnaroshi <gnaroshi@gmail.com>
2025-10-04 23:07:46 +0900

    20251004 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_4/17028.cpp
@@ -0,0 +1,59 @@
+// problem: Sleepy Cow Sorting
+// id: 17028
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, ans;
+  const int SZ = 200;
+  cin >> n;
+  vi v(n);
+  for (auto &i : v) {
+    cin >> i;
+  }
+  for (int i = n - 1; i > 0; i--) {
+    if (v[i - 1] > v[i]) {
+      ans = i;
+      break;
+    }
+  }
+
+  cout << ans << '\n';
+
+  return 0;
+}
