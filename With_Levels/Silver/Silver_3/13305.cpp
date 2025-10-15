f172631b3145f34103179c5731e3fc49d449db75
gnaroshi <gnaroshi@gmail.com>
2025-10-15 15:46:12 +0900

    20251015 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_3/13305.cpp
@@ -0,0 +1,58 @@
+// problem: 주유소
+// id: 13305
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll n, t, ans = 0, mn;
+  cin >> n;
+  vll d(n);
+  for (int i = 1; i < n; i++) {
+    cin >> d[i];
+  }
+  cin >> mn;
+  for (int i = 1; i < n; i++) {
+    cin >> t;
+    ans += d[i] * mn;
+    mn = min(mn, t);
+  }
+
+  cout << ans << "\n";
+
+  return 0;
+}
