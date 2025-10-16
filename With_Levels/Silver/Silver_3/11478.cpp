a92dd3a339a42ba946f6f8265e3d9712a6987d24
gnaroshi <gnaroshi@gmail.com>
2025-10-16 14:46:36 +0900

    20251016 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_3/11478.cpp
@@ -0,0 +1,56 @@
+// problem: 서로 다른 부분 문자열의 개수
+// id: 11478
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  map<string, bool> mp;
+  string s;
+  cin >> s;
+  int iter = s.length();
+  for (int i = 1; i <= iter; i++) {
+    int cl = iter - i + 1;
+    for (int j = 0; j < cl; j++) {
+      mp[s.substr(j, i)] = true;
+    }
+  }
+
+  cout << mp.size() << '\n';
+
+  return 0;
+}
