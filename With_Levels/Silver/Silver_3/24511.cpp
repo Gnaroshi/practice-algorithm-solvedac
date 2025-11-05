6a2cda729410d355efa742276ce645cf2cf97e69
gnaroshi <gnaroshi@gmail.com>
2025-11-05 11:32:53 +0900

    20251105 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_3/24511.cpp
@@ -0,0 +1,68 @@
+// problem: queuestack
+// id: 24511
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m, cur, nxt;
+  cin >> n;
+  vb qs(n);
+  deque<int> dq;
+  bool tqs;
+  for (int i = 0; i < n; i++) {
+    cin >> tqs;
+    qs[i] = tqs;
+  }
+
+  for (int i = 0; i < n; i++) {
+    cin >> cur;
+    if (!qs[i]) {
+      dq.push_back(cur);
+    }
+  }
+
+  cin >> m;
+  for (int i = 0; i < m; i++) {
+    cin >> nxt;
+    dq.push_front(nxt);
+    cout << dq.back() << ' ';
+    dq.pop_back();
+  }
+
+  return 0;
+}
