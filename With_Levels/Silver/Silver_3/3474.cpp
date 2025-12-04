42fecc76fac828f3353461f7cf1e87b42ed3ac34
gnaroshi <gnaroshi@gmail.com>
2025-12-04 12:58:17 +0900

    202511204 [UPDATE] solvedac practice - mac

---
+++ With_Levels/Silver/Silver_3/3474.cpp
@@ -0,0 +1,60 @@
+// problem: 교수가 된 현우
+// id: 3474
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int n, ans = 0, d = 5;
+    cin >> n;
+
+    while (true) {
+      if (d > n) {
+        break;
+      }
+      ans += (n / d);
+      d *= 5;
+    }
+
+    cout << ans << '\n';
+  }
+
+  return 0;
+}
