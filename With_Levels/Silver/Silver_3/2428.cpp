ad97122ec3af47451c19f2eea39afd95b4a3fbe6
gnaroshi <gnaroshi@gmail.com>
2026-02-01 21:25:14 +0900

    20260201 [UPDATE]solvedac practice - mac

---
+++ With_Levels/Silver/Silver_3/2428.cpp
@@ -0,0 +1,63 @@
+// problem: 표절
+// id: 2428
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll ans = 0;
+  int n;
+  cin >> n;
+  vi v(n);
+  for (auto &i : v) {
+    cin >> i;
+  }
+
+  sort(v.begin(), v.end());
+  int j = 0;
+  for (int i = 0; i < n; i++) {
+    while (j + 1 < n && v[i] * 10 >= v[j + 1] * 9) {
+      j++;
+    }
+    if (j > i) {
+      ans += (j - i);
+    }
+  }
+  cout << ans << '\n';
+
+  return 0;
+}
