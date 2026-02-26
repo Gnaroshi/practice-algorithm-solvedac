a03a389b4379acfda46db9fc6917c433181136e2
gnaroshi <gnaroshi@gmail.com>
2026-02-26 14:16:40 +0900

    20260226 [UPDATE]solvedac practice - mac

---
+++ With_Levels/Silver/Silver_3/2121.cpp
@@ -0,0 +1,62 @@
+// problem: 넷이 놀기
+// id: 2121
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll n, a, b, cnt = 0, cx, cy;
+  cin >> n >> a >> b;
+  vpll v(n);
+  for (auto &i : v) {
+    cin >> i.first >> i.second;
+  }
+  sort(v.begin(), v.end());
+  for (auto i : v) {
+    cx = i.first, cy = i.second;
+    if (binary_search(v.begin(), v.end(), make_pair(cx + a, cy)) &&
+        binary_search(v.begin(), v.end(), make_pair(cx, cy + b)) &&
+        binary_search(v.begin(), v.end(), make_pair(cx + a, cy + b))) {
+      cnt++;
+    }
+  }
+
+  cout << cnt << '\n';
+
+  return 0;
+}
