41c8f1d13347b34171356631250d9995ca687a4e
gnaroshi <gnaroshi@gmail.com>
2026-04-02 00:09:14 +0900

    20260401 [UPDATE]solvedac practice - mac

---
+++ With_Levels/Silver/Silver_3/23561.cpp
@@ -0,0 +1,56 @@
+// problem: Young한 에너지는 부족하다
+// id: 23561
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, mn, mx;
+  cin >> n;
+  vi v(n * 3);
+  for (auto &i : v) {
+    cin >> i;
+  }
+
+  sort(v.begin(), v.end());
+
+  mn = v[n];
+  mx = v[n + n - 1];
+  cout << mx - mn << '\n';
+
+  return 0;
+}
