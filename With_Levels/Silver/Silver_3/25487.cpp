6ce4815384df6955f14cb7a6c1b8ef5706250b2c
gnaroshi <gnaroshi@gmail.com>
2026-04-09 00:37:53 +0900

    20260408 [UPDATE]solvedac practice - mac

---
+++ With_Levels/Silver/Silver_3/25487.cpp
@@ -0,0 +1,51 @@
+// problem: 단순한 문제 (Large)
+// id: 25487
+// tag:
+// time taken:
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << min({a, b, c}) << '\n';
+  }
+
+  return 0;
+}
